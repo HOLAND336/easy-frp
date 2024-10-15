f16f2fb7aca8f61aeab722c2db72b1f8d4d6537d
HOLAND336 <holand336@foxmail.com>
2024-10-15 09:54:11 +0800

    Update easy-frp.sh

--- easy-frp.sh
+++ easy-frp.sh
@@ -5,6 +5,17 @@ red="\e[31m"
 yellow="\e[33m"
 reset="\e[0m"
 
+#当前脚本版本
+script_version=1.0.2
+
+#最新脚本版本获取
+get_latest_script_version(){
+    latest_script_version=$(curl -s https://api.github.com/repos/HOLAND336/easy-frp/releases/latest | grep '"tag_name"' | sed -E 's/.*"v?([^"]+)".*/\1/')
+}
+
+get_latest_script_version
+
+
 #最新版本获取
 get_latest(){
     latest=$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep 'html_url": "https://github.com/fatedier/frp/releases/tag' | sed -n 's/.*tag\/v\([0-9.]*\).*/\1/p')
@@ -24,22 +35,34 @@ get_version() {
 get_version
 
 
+
+
+
+
+
+
+
+
+
+
 # 菜单主页面
 menu() {
     while true; do
         get_version
         echo -e "当前最新版本 FRP: ${green}$latest${reset}"
         echo -e "已安装FRP版本：$version"
-        echo "FRP安装菜单"
+        echo -e "FRP安装菜单 ${green}$script_version${reset}"
         echo "1. 安装/更新 FRP"
         echo "2. 开启自启 客户端/服务端"
         echo "3. 关闭自启 客户端/服务端"
-        echo "4. FRP 状态检查"
-        echo "5. 卸载 FRP"
+        echo "4. 重新启动 客户端/服务端"
+        echo "5. FRP 状态检查"
+        echo "6. 管理脚本更新"
+        echo "7. 卸载 FRP"
         echo "0. 退出"
         
         # 提示用户输入
-        read -p "请输入一个0-5之间的数字：" choice_01
+        read -p "请输入一个0-7之间的数字：" choice_01
 
         # 根据用户的输入执行相应的操作
         case $choice_01 in
@@ -47,9 +70,11 @@ menu() {
             1) install ;;
             2) enable ;;
             3) disable ;;
-            4) status ;;
-            5) uninstall ;;
-            *) echo "请输入0-5之间的数字" ;;
+            4) restart ;;
+            5) status ;;
+            6) script_update ;;
+            7) uninstall ;;
+            *) echo "请输入0-7之间的数字" ;;
         esac
     done
 }
@@ -74,6 +99,55 @@ update() {
     read -n 1 -s -r -p "按任意键继续..."
     get_version
 }
+
+
+
+
+#输入frp执行脚本
+script_up(){
+    sudo cp -f /root/frp_install.sh /usr/local/bin/frp
+    sudo cp -f /root/frp_install.sh /usr/local/bin/FRP    
+    sudo chmod +x /usr/local/bin/frp
+    sudo chmod +x /usr/local/bin/FRP
+    sudo chmod +x /root/frp_install.sh
+    return
+}
+
+#脚本更新
+script_update(){
+    get_latest_script_version
+    echo -e "当前最新脚本版本为：${green}$latest_script_version${reset}"
+    echo -e "当前脚本版本为：${green}$script_version${reset}"
+    while true; do
+        echo "是否要更新"
+        echo "请输入Y/n"
+        read input2
+        case $input2 in
+            Y|y) break ;;
+            N|n) return ;;
+            *) echo "请输入Y/n" ;;
+        esac
+    done
+    sudo wget -O /root/frp_install.sh "https://github.com/HOLAND336/easy-frp/releases/download/v${latest_script_version}/frp_install.sh"
+    if [[ $? -eq 0 ]]; then
+        
+        # 更新脚本路径和权限
+        script_up
+        
+        echo "脚本更新成功"
+        sleep 1
+        # 重新执行最新的脚本
+        exec frp
+    else
+        # 下载失败，保留原脚本
+        echo -e "${red}脚本下载失败，请检查网络或GitHub地址！${reset}"
+        sudo rm -f /root/frp_install.sh
+    fi
+    return
+
+
+}
+
 
 
 #安装模块
@@ -111,6 +185,8 @@ install() {
             mkdir -p /etc/frp/
             mv -f /root/frp_tmp/frp_${latest}_linux_amd64/* /etc/frp/
             system_install
+
+
             echo "安装完成"
             menu
         else #其他异常情况
@@ -154,6 +230,8 @@ uninstall() {
     sudo systemctl disable frps
     sudo rm -rf /etc/systemd/system/frps.service
     sudo rm -rf /etc/frp
+    sudo rm -rf /usr/local/bin/frp
+    sudo rm -rf /usr/local/bin/FRP 
     echo "卸载完成"
     read -n 1 -s -r -p "按任意键继续..."
 	echo -e "\n"
@@ -296,6 +374,53 @@ WantedBy=multi-user.target"
 }
 
 
+
+restart() {
+    while true; do
+        echo "输入 1 重启客户端"
+        echo "输入 2 重启服务端"
+        echo "输入 0 返回主菜单"
+        read -e -p "请选择 (1 重启客户端, 2 重启服务端, 0 返回主菜单): " input3
+        case $input3 in
+            1) restart_c 
+               return ;;
+            2) restart_s
+               return ;;
+            0) return ;;
+            *) echo -e "请输入数字0-2" ;;
+        esac
+    done
+}
+
+
+
+restart_c() {
+    clear
+    echo "正在重启客户端..."
+    sudo systemctl restart frpc
+    seperator
+    sudo systemctl status frpc
+    seperator
+    sleep 2
+    clear
+    return
+}
+
+
+
+restart_s() {
+    clear
+    echo "正在重启服务端..."
+    sudo systemctl restart frps
+    seperator
+    sudo systemctl status frps
+    seperator
+    sleep 2
+    clear
+    return
+}
+
+
 status() {
     seperator
     echo "客户端状态"
@@ -315,6 +440,17 @@ status() {
 seperator() {
     printf '%*s\n' "${COLUMNS:-80}" '' | tr ' ' '-'
 }
+
+
+
+#第一次自动安装脚本
+if [[ ! -f /usr/local/bin/frp || ! -f /usr/local/bin/FRP ]]; then
+    echo "检测到第一次使用，正在安装脚本面板..."
+    script_up
+    echo "面板安装成功！"
+    echo -e "后面输入 ${green}frp${reset} 即可进入面板"
+    read -n 1 -s -r -p "按任意键继续..."
+fi
 
 
 menu
